f957f461f244e3bff791e7427a095aaae1b5830e
Erion Polisi <Erion.Polisi@msg-plaut.com>
2026-06-30 03:45:30 +0200

    DbContext init

--- backend/TourPlanner.API/Program.cs
+++ backend/TourPlanner.API/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TourPlanner.DataAccessLayer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<TourPlannerDbContext>( opt => opt.UseNpgsql(connectionString));
 
 var app = builder.Build();
 
